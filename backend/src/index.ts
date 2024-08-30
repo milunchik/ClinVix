12401b75999e3c7a521e5ed8377c8c7e4563e209
Emily <emili061116@gmail.com>
2024-08-30 23:02:20 +0300

    Initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.listen(PORT, () => console.log(`Server started http://localhost:5000`));
